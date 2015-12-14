40f88c9c2e876905c72931c6bde77f80cf200a21
jaimegag <jaguilar@pivotal.io>
2015-12-14 12:51:40 -0500

    view for index and killswitch

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'redis'
 require 'cf-app-utils'
+require 'json'
+require 'net/http'
 
 before do
   unless redis_credentials
@@ -12,6 +14,42 @@ You can run the following commands to create an instance and bind to it:
   $ cf create-service p-redis development redis-instance
   $ cf bind-service <app-name> redis-instance})
   end
+end
+
+get '/' do
+  settings.requests += 1
+  app_info = ENV['VCAP_APPLICATION'] ? JSON.parse(ENV['VCAP_APPLICATION']) : Hash.new
+  ENV['APP_NAME'] = app_info["application_name"]
+  ENV['APP_INSTANCE'] = app_info["instance_index"].to_s
+  ENV['APP_MEM'] = app_info["limits"] ? app_info["limits"]["mem"].to_s : " "
+  ENV['APP_DISK'] = app_info["limits"] ? app_info["limits"]["disk"].to_s : " "
+  ENV['APP_IP'] = IPSocket.getaddress(Socket.gethostname)
+  ENV['APP_PORT'] = app_info["port"].to_s
+  ENV['SERVICE_JSON'] = JSON.pretty_generate(JSON.parse(ENV['VCAP_SERVICES']))
+  erb :'index'
+end
+
+get '/killSwitch' do
+  Kernel.exit!
+end
+
+get '/load' do
+   i = 0
+   myStr = "Kill the CPU!!!"
+   buff = ""
+
+  while i < 50000  do
+    buff += myStr.to_s
+    buff.reverse!
+    i += 1
+  end
+  settings.requests += 1
+  "<h2>I'm healthy!</h2>"
+end
+
+get '/health' do
+  settings.requests += 1
+  "<h2>I'm healthy!</h2>"
 end
 
 put '/:key' do
